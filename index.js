500368ace9a5438c0b675d21ee2bb0b23983ab87
Braddock20 <braddockikubano@gmail.com>
2025-07-18 17:43:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
-
 const videoInfo = require('./routes/videoInfo');
-const stream = require('./routes/stream');
 const audio = require('./routes/audio');
+const stream = require('./routes/stream');
 const download = require('./routes/download');
 
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.use('/videoInfo', videoInfo);
-app.use('/stream', stream);
 app.use('/audio', audio);
+app.use('/stream', stream);
 app.use('/download', download);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ API running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
