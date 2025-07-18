37f9bad629a328f93298962158275b3d2b73e86e
Braddock20 <braddockikubano@gmail.com>
2025-07-18 20:33:32 +0300

    Delete proxies.js

--- proxies.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  'http://51.158.123.35:8811',
-  'http://103.145.31.250:8080',
-  'http://38.154.227.167:6007',
-  'http://47.243.180.142:808',
-  'http://154.236.191.246:1976',
-  // Add more from public proxy sites (rotate weekly)
-];
